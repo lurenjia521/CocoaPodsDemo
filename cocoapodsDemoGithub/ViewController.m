1c238d5779e577240daf9f4e9b0221d45f614d19
535755455@qq.com <535755455@qq.com>
2018-10-24 09:13:20 +0800

    feat:尝试引用

--- cocoapodsDemoGithub/ViewController.m
+++ cocoapodsDemoGithub/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "UIView+KBCategory.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    NSLog(@"%f",self.view.kb_height);
+
 }
 
 
